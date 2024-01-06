a7402d37891b3ed1ebbcc8ece517b8dc516e76ad
Maksym45 <maks.meleshko12@gmail.com>
2024-01-06 16:46:40 +0200

    added_sidebar_components

--- src/Components/ImageGenerate/ImageGenerator.jsx
+++ src/Components/ImageGenerate/ImageGenerator.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from 'react'
 import './ImageGenerator.css'
-import default_image from '../Assets/default_image.svg'
+import default_image from '../Assets/default_image.jpg'
 
 const ImageGenerator = () => {
 
